73047a7d978a8b17a0741f13af909bfe7169dbd5
Cristopher Coronado Moreira <cristofima@hotmail.com>
2019-04-13 10:31:27 -0500

    Initial commit

---
+++ SignalRChatBasic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRChatBasic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
